29bdbc4dac0f3d4c74abfba0f1dfe2cca1c0ccf5
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-02-08 11:07:03 +0100

    Add files via upload

---
+++ src/routes/index.tsx
@@ -0,0 +1,35 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div>
+      <h1>
+        Welcome to Qwik <span class="lightning">⚡️</span>
+      </h1>
+
+      <ul>
+        <li>
+          Check out the <code>src/routes</code> directory to get started.
+        </li>
+        <li>
+          Add integrations with <code>npm run qwik add</code>.
+        </li>
+        <li>
+          More info about development in <code>README.md</code>
+        </li>
+      </ul>
+
+          </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Welcome to Qwik',
+  meta: [
+    {
+      name: 'description',
+      content: 'Qwik site description',
+    },
+  ],
+};
---
+++ src/routes/layout.tsx
@@ -0,0 +1,26 @@
+import { component$, Slot } from '@builder.io/qwik';
+
+export default component$(() => {
+  return (
+    <>
+      <nav>
+        <h1>Header</h1>
+      </nav>
+      <section class='row'>
+        <div class='column menu-items'>
+          <h2>Menú</h2>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+          </ul>
+        </div>
+        <div class='column content'>
+          <Slot />
+        </div>
+      </section>
+      <footer>
+        <h2>Footer</h2>
+      </footer>
+    </>
+  );
+});
