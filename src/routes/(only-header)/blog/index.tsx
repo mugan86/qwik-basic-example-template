6b132bfd43441de6806bb00882c1f90a99cc7be9
Anartz Mugika Ledo <mugan86@gmail.com>
2023-02-11 20:00:28 +0100

    feat: implement grouped layouts

---
+++ src/routes/(only-header)/blog/index.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    return (
+      <div>
+        <h1>Ruta "/blog"</h1>
+        Esta es la página <code>blog</code> con la plantilla <code>only-header</code>
+      </div>
+    );
+  });
+  
