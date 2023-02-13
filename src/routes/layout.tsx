d150a5d03488d46ba8ba49cef2e5f5c3b7c3fb9d
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-02-13 10:04:52 +0100

    feat: nested-layouts base template to start working in section

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,19 +1,12 @@
 import { component$, Slot } from '@builder.io/qwik';
-import { Footer } from '~/components/core/footer';
-import { Header } from '~/components/core/header';
-import { Menu } from '~/components/core/menu';
 
 export default component$(() => {
   return (
-    <>
-      <Header />
-      <section class='row'>
-        <Menu />
-        <div class='column content'>
-          <Slot />
-        </div>
-      </section>
-      <Footer />
-    </>
+    <div>
+      <p>
+        <code>src/routes/layout.tsx</code>
+      </p>
+      <Slot />
+    </div>
   );
 });
