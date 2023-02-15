f2cd9094d4d4df1ff01018d7628350b08dd07c06
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-02-15 07:16:55 +0100

    feat: implement base code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,19 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { LeafletMap } from '~/components/map';
 
 export default component$(() => {
   return (
     <div>
-      <h1>Leaflet - Qwik</h1>
-      <p>Ejemplo de un mapa Leaflet en Qwik</p>
-      <p>Pasos para configurar la librería y poder trabajar con Leaflet en Qwik</p>
+      <h1>Leaflet - Styles</h1>
+      <p>Trabajaremos con los estilos</p>
+      <p>Todos los artículos con los que hemos trabajado: <a href="https://medium.com/@mugan86/list/qwik-paso-a-paso-desde-0-al-detalle-e7df8b471166" target="_blank">Medium</a></p>
       <ol>
-        <li>Instalar las dependencias de Leaflet: <code>npm i leaflet@1.9.3</code></li>
-        <li>Instalar las dependencias de types de Leaflet: <code>npm i -D @types@leaflet</code></li>
-        <li>En <code>src/root.tsx</code>añadimos el siguiente código para añadir los estilos de Leaflet que son necesarios<br/><br/>
-          <img src="leaflet-use-styles.png" width="40%"/>
-        </li>
-        <li>Y configurar lo que se muestra <a href="https://leafletjs.com/examples/quick-start/" target="_blank">aquí como referencia</a> en el componente <b>LeafletMap</b> que se encuentra en <code>src/components/map/index.tsx</code> y conseguiremos trabajar con Leaflet :)</li>
+        <li>Introducción a los estilos</li>
+        <li>Estilos globales</li>
+        <li>Estilos por componente: Uso de useStyles$ y useStylesScoped$</li>
+        <li>CSS en Javascript</li>
+        <li>Styled Components (Componentes con estilo)</li>
       </ol>
-      <LeafletMap></LeafletMap>
     </div>
   );
 });
