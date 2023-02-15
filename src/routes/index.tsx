e4ac8229074f06045be4896d38fdd4d47be7a7fe
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-02-15 10:35:24 +0100

    feat: useStyles finish

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,37 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$, useStore, useStyles$ } from '@builder.io/qwik';
+import { DocumentHead, Link } from '@builder.io/qwik-city';
+export const PlusInfoCss = `
+.plus-info {
+  margin-top: 1em;
+  border: 1px solid red;
+  padding: 1em;
+  display: block;
+}
+`;
+export const PlusInfo = component$(() => {
+  useStyles$(PlusInfoCss);
+  return (
+    <div class='plus-info'>
+      <div>More info...</div>
+    </div>
+  );
+});
 
 export default component$(() => {
+  const store = useStore({ open: false });
   return (
     <div>
       <h1>Leaflet - Styles</h1>
       <p>Trabajaremos con los estilos</p>
-      <p>Todos los artículos con los que hemos trabajado: <a href="https://medium.com/@mugan86/list/qwik-paso-a-paso-desde-0-al-detalle-e7df8b471166" target="_blank">Medium</a></p>
+      <p>
+        Todos los artículos con los que hemos trabajado:{' '}
+        <a
+          href='https://medium.com/@mugan86/list/qwik-paso-a-paso-desde-0-al-detalle-e7df8b471166'
+          target='_blank'
+        >
+          Medium
+        </a>
+      </p>
       <ol>
         <li>Introducción a los estilos</li>
         <li>Estilos globales</li>
@@ -14,6 +39,12 @@ export default component$(() => {
         <li>CSS en Javascript</li>
         <li>Styled Components (Componentes con estilo)</li>
       </ol>
+      {store.open ? <PlusInfo /> : null}
+      <button onClick$={() => (store.open = !store.open)}>
+        Show Plus Info
+      </button>
+      <br />
+      <Link href='/second'>Second Page</Link>
     </div>
   );
 });
